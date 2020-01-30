60db9ce36f026806f96dceb54867da07159c29fe
Юлия Корнейчук <uliakorneychuk@MacBook-Air.local>
2020-01-30 23:51:40 +0300

    Разбивает renderCloud на 2 функции

--- js/stat.js
+++ js/stat.js
@@ -5,29 +5,39 @@ var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
 var GAP = 10;
+var TITLE_COLOR = '#000';
+var TITLE_FONT = '16px PT Mono';
+var TITLE_TEXT = 'Ура вы победили! \nСписок результатов:';
+// цвета облака
+var shadow = 'rgba(0, 0, 0, 0.7)';
+var white = '#fff';
 
-var renderCloud = function (ctx, x, y, color) {
-  ctx.fillStyle = color;
-  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+var drawCloud = function (ctx) {
+  ctx.fillStyle = white;
+  ctx.fillRect(CLOUD_X, CLOUD_Y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-// var renderTitle = function (ctx) {
-// ctx.fillStyle = '#000';
-// ctx.font = '16px PT Mono';
-// ctx.fillText('Ура вы победили!', 130, 50);
-// ctx.fillText('Список результатов:', 130, 70);
-// };
+var drawShadow = function (ctx) {
+  ctx.fillStyle = shadow;
+  ctx.fillRect(CLOUD_X + GAP, CLOUD_Y + GAP, CLOUD_WIDTH, CLOUD_HEIGHT);
+};
 
-
-window.renderStatistics = function (ctx) {
-  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
-  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
-  renderTitle(ctx, 130, 50, 'Ура вы победили!', '#000', '16px PT Mono');
-  renderTitle(ctx, 130, 70, 'Список результатов:', '#000', '16px PT Mono');
+var renderCloud = function (ctx) {
+  drawShadow(ctx);
+  drawCloud(ctx);
 };
 
 var renderTitle = function (ctx, x, y, text, color, font) {
   ctx.font = font;
   ctx.fillStyle = color;
-  ctx.fillText(x, y, text);
+  ctx.fillText(text, x, y);
 };
+
+window.renderStatistics = function (ctx) {
+  renderCloud(ctx);
+  renderTitle(ctx, 130, 50, TITLE_TEXT, TITLE_COLOR, TITLE_FONT);
+};
+
+// функция рандомного цвета js
+
+
