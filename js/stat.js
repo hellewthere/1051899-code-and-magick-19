2b39002830237a2422a0631815e02059c53545a8
Юлия Корнейчук <uliakorneychuk@MacBook-Air.local>
2020-02-01 22:24:35 +0300

    Заполняет рандомным цветом блоки

--- js/stat.js
+++ js/stat.js
@@ -1,20 +1,37 @@
 'use strict';
 
+// параметры облака
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
-var GAP = 10;
+
+// параметры текста
 var TITLE_COLOR = '#000';
 var TITLE_FONT = '16px PT Mono';
-var TITLE_TEXT = 'Ура вы победили! \nСписок результатов:';
-// цвета облака
+var TITLE_TEXT = 'Ура вы победили!';
+var TITLE_MSG = 'Список результатов:';
+
+// параметры цвета
 var shadow = 'rgba(0, 0, 0, 0.7)';
 var white = '#fff';
-var BLOCK_X = CLOUD_X + GAP;
-var BLOCK_WIDTH = 40;
-var BLOCK_HEIGHT = 150;
+var blockColor = 'rgba(255, 0, 0, 1)';
 
+// вспомогательные параметры
+var GAP = 10;
+var OFFSET = BLOCK_WIDTH + 50;
+
+// параметры блока
+var BLOCK_X = 160;
+var BLOCK_Y = 240;
+var BLOCK_WIDTH = 40;
+var BLOCK_HEIGHT = -150;
+
+
+var renderCloud = function (ctx) {
+  drawShadow(ctx);
+  drawCloud(ctx);
+};
 
 var drawCloud = function (ctx) {
   ctx.fillStyle = white;
@@ -26,33 +43,32 @@ var drawShadow = function (ctx) {
   ctx.fillRect(CLOUD_X + GAP, CLOUD_Y + GAP, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-var renderCloud = function (ctx) {
-  drawShadow(ctx);
-  drawCloud(ctx);
+var renderTitle = function (ctx) {
+  ctx.font = TITLE_FONT;
+  ctx.fillStyle = TITLE_COLOR;
+  ctx.fillText(TITLE_TEXT, 130, 60);
+  ctx.fillText(TITLE_MSG, 130, 40);
 };
 
-var renderTitle = function (ctx, x, y, text, color, font) {
-  ctx.font = font;
-  ctx.fillStyle = color;
-  ctx.fillText(text, x, y);
+// отрисовать гистограмму
+window.renderStatistics = function (ctx, names, times) {
+  renderCloud(ctx);
+  renderTitle(ctx);
+  renderBlocks(ctx, names, times);
 };
 
 var renderBlocks = function (ctx, names, times) {
-  window.console.log(names);
-  window.console.log(times);
-  fillBlocks();
-
   var maxTime = getMaxElement(times);
 
-  console.log(names);
-  console.log(times);
-  console.log(maxTime);
-};
+  for (var i = 0; i < times.length; i++) {
 
-window.renderStatistics = function (ctx, names, times) {
-  renderCloud(ctx);
-  renderTitle(ctx, 130, 50, TITLE_TEXT, TITLE_COLOR, TITLE_FONT);
-  renderBlocks(ctx, names, times);
+    ctx.fillStyle = getRandomColor();
+    if (names[i] === 'Вы') {
+      ctx.fillStyle = blockColor;
+    }
+
+    ctx.fillRect(BLOCK_X + OFFSET * i, BLOCK_Y, BLOCK_WIDTH, BLOCK_HEIGHT * times[i] / maxTime);
+  }
 };
 
 // найти максимальное значение массива times
@@ -68,12 +84,10 @@ var getMaxElement = function (arr) {
   return maxElement;
 };
 
-// ищем цвет блока в зависимости от имени
-function fillBlocks(ctx, namePlayer) {
-  var randomOpacity = Math.random().toFixed(2);
-  if (namePlayer === 'Вы') {
-    ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-  } else {
-    ctx.fillStyle = 'rgba(0, 0, 255, ' + randomOpacity + ')';
-  }
-}
+// функция рандомного цвета
+var getRandomColor = function () {
+  var str1 = 'rgba(0, 0, 255,';
+  var str2 = Math.random().toPrecision(1);
+  var str3 = ')';
+  return str1 + str2 + str3;
+};
