0e7b4fb0884e5306a35c5462c35df18ae0bdaf49
Юлия Корнейчук <uliakorneychuk@MacBook-Air.local>
2020-02-07 00:27:32 +0300

    Распределяет код по функциям

--- js/setup.js
+++ js/setup.js
@@ -1,26 +1,11 @@
 'use strict';
 
-document.querySelector('.setup').classList.remove('hidden');
-document.querySelector('.setup-similar').classList.remove('hidden');
-
-var similarListElement = document.querySelector('.setup-similar-list');
-var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
-
 var names = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-
 var surnames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-
-var coatColors = [
-  'rgb(101, 137, 164)',
-  'rgb(241, 43, 107)',
-  'rgb(147, 100, 161)',
-  'rgb(56, 159, 117)',
-  'rgb(215, 210, 55)',
-  'rgb(0, 0, 0)',
-];
+var coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(147, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
 
-var createWizard = function () {
+var createPerson = function () {
   var wizardObj = {
     name: names[Math.floor(Math.random() * names.length)],
     surname: surnames[Math.floor(Math.random() * surnames.length)],
@@ -33,35 +18,43 @@ var createWizard = function () {
 
 var renderWizardsData = function () {
   var data = [];
-
+  // 4 итерации цикла генерируют 4 персонажей
   for (var i = 0; i < 4; i++) {
-    var wizard = createWizard();
+    var wizard = createPerson();
     data.push(wizard);
   }
-
   return data;
 };
-
-
+// переменная получает результат работы функции
 var myWizards = renderWizardsData();
 
-
-for (var i = 0; i < myWizards.length; i++) {
+var createWizard = function (wizardObj) {
+  var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
   var wizardElement = similarWizardTemplate.cloneNode(true);
-  console.log(myWizards[i]);
-  // На основе данных, созданных в предыдущем пункте и шаблона #similar-wizard-template
-  // создайте DOM-элементы, соответствующие случайно сгенерированным волшебникам, и заполните
-  // их данными из массива
-  wizardElement.querySelector('.setup-similar-label').textContent = myWizards[i].name + ' ' + myWizards[i].surname;
-  wizardElement.querySelector('.wizard-coat').style.fill = myWizards[i].coatColor;
-  wizardElement.querySelector('.wizard-eyes').style.fill = myWizards[i].eyesColor;
 
-  // Отрисуйте сгенерированные DOM-элементы в блок .setup-similar-list.
-  // Для вставки элементов используйте DocumentFragment.
-  // eslint-disable-next-line no-unused-vars
-  var fragment = document.createDocumentFragment('.setup-similar-list');
+  wizardElement.querySelector('.setup-similar-label').textContent = wizardObj.name + ' ' + wizardObj.surname;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizardObj.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizardObj.eyesColor;
 
-  similarListElement.appendChild(wizardElement);
-}
+  return wizardElement;
+};
 
+var renderWizards = function (wizardsData) {
+  var similarListElement = document.querySelector('.setup-similar-list');
+  var fragment = document.createDocumentFragment();
+
+  for (var i = 0; i < wizardsData.length; i++) {
+    fragment.appendChild(createWizard(wizardsData[i]));
+  }
+  similarListElement.appendChild(fragment);
+};
+
+// Открывает окно с волшебниками
+var openPopup = function () {
+  document.querySelector('.setup').classList.remove('hidden');
+  document.querySelector('.setup-similar').classList.remove('hidden');
+};
+
+renderWizards(myWizards);
+openPopup();
 
