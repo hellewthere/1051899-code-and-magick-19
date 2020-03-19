12db79610875770bf534768704dc4860515e40e4
Юлия Корнейчук <uliakorneychuk@MacBook-Air.local>
2020-03-19 16:27:16 +0300

    Обработка возможных ошибок

--- js/backend.js
+++ js/backend.js
@@ -59,8 +59,22 @@
     xhr.send(data);
   };
 
+  // обработка возможных ошибок
+  var onError = function (errorMessage) {
+    var node = document.createElement('div');
+    node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
+    node.style.position = 'absolute';
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fontSize = '35px';
+
+    node.textContent = errorMessage;
+    document.body.insertAdjacentElement('afterbegin', node);
+  };
+
   window.backend = {
     load: load,
     save: save,
+    onError: onError
   };
 })();
